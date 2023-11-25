4ce12915ccec971120d6aba1c793963f5054c428
Divy Agrawal <divyagrawal29@gmail.com>
2023-11-25 13:05:36 +0530

    Advanced Features Initial Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
-function EnterNote(currentNote) {
-  return (
-    <Note
-      key={currentNote.key}
-      title={currentNote.title}
-      content={currentNote.content}
-    />
-  );
-}
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(EnterNote)}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 function Footer() {
-  var date = new Date();
-  var year = date.getFullYear();
+  const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ©{year}</p>
+      <p>Copyright ⓒ {year}</p>
     </footer>
   );
 }
